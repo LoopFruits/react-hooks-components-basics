e7ddf582cdac6ebef1ecd9b832a11d2a60d3944a
LoopFruits <Kensworldc@gmail.com>
2022-02-24 09:58:13 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Comment from "./Comment";
 function App() {
   return (
     <div>
-      <Article />
+      <Article /> 
       <Comment />
     </div>
   );
 }
 
 export default App;
+
+// in we have to add the components from our other javascript files. We add it here because this is our app 
+
+
+// components need to be in order as well 
+
+// we have to capitalize the components otherwise they will be rendered as HTML elements 
